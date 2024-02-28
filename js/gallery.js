524bbcb1ac74dd6223cf9c878b0885604a45d6d6
Ruslan <monnaliza139@gmail.com>
2024-02-28 23:17:55 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,6 @@
 const images = [
-  {
+  {
+    id:1,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
     original:
@@ -7,7 +8,8 @@ const images = [
         description: 'Hokkaido Flower',
         
   },
-  {
+  {
+    id:2,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
@@ -15,7 +17,8 @@ const images = [
       description: 'Container Haulage Freight',
     
   },
-  {
+  {
+    id:3,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
     original:
@@ -23,7 +26,8 @@ const images = [
       description: 'Aerial Beach View',
     
   },
-  {
+  {
+    id:4,
     preview:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
@@ -31,14 +35,16 @@ const images = [
       description: 'Flower Blooms',
     
   },
-    {
+  {
+      id:5,
         preview:
             'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
         original:
             'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
         description: 'Alpine Mountains',
     }, 
-  {
+  {
+    id:6,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
     original:
@@ -46,7 +52,8 @@ const images = [
       description: 'Mountain Lake Sailing',
     
   },
-  {
+  {
+    id:7,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
     original:
@@ -54,7 +61,8 @@ const images = [
       description: 'Alpine Spring Meadows',
     
   },
-  {
+  {
+    id:8,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
     original:
@@ -62,7 +70,7 @@ const images = [
       description: 'Nature Landscape',
    
   },
-  {
+  {id:9,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
@@ -73,12 +81,12 @@ const images = [
 ];
 const { preview, original, description } = images;
 const imagesListItem = document.querySelector('.gallery')
-function imagesTemplate({preview, original, description}){
-     return `<li class="gallery-item">
+function imagesTemplate({preview, original, description, id}){
+     return `<li class="gallery-item" data-id ="${id} >
   <a class="gallery-link" href="${original}"> 
     <img class="gallery-image"
       src="${preview}"
-      data-source="${original}"
+      data-source="${preview}"
       alt="${description}"
     />
   </a>
@@ -97,11 +105,12 @@ const galleryContainer = document.querySelector('.gallery');
 galleryContainer.addEventListener('click', e => {
     e.preventDefault()
     if (e.target === e.currentTarget) return;
-     const preview = e.target.getAttribute('data-source');
+  const imgId = e.target.getAttribute('data-id');
+  
   
     
 const instance = basicLightbox.create(`
-    <img src="${preview}" width="1112" height="640">
+    <img src="${preview}" width="1112" height="640" data-id ="${id}>
 `,
  {
      onShow: instance => {
