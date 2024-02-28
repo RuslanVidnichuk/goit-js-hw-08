8b908b677f5aed037dc6c33f93e5f86fdf62fc71
Ruslan <monnaliza139@gmail.com>
2024-02-28 23:54:11 +0200

    update__2

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,5 @@
 const images = [
   {
-    id:1,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
     original:
@@ -9,7 +8,6 @@ const images = [
         
   },
   {
-    id:2,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
@@ -18,7 +16,6 @@ const images = [
     
   },
   {
-    id:3,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
     original:
@@ -27,7 +24,6 @@ const images = [
     
   },
   {
-    id:4,
     preview:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
@@ -36,7 +32,6 @@ const images = [
     
   },
   {
-      id:5,
         preview:
             'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
         original:
@@ -44,7 +39,6 @@ const images = [
         description: 'Alpine Mountains',
     }, 
   {
-    id:6,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
     original:
@@ -53,7 +47,6 @@ const images = [
     
   },
   {
-    id:7,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
     original:
@@ -62,7 +55,6 @@ const images = [
     
   },
   {
-    id:8,
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
     original:
@@ -70,8 +62,8 @@ const images = [
       description: 'Nature Landscape',
    
   },
-  {id:9,
-    preview:
+  {
+    preview:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
@@ -81,12 +73,12 @@ const images = [
 ];
 const { preview, original, description } = images;
 const imagesListItem = document.querySelector('.gallery')
-function imagesTemplate({preview, original, description, id}){
-     return `<li class="gallery-item" data-id ="${id} >
+function imagesTemplate({preview, original, description,}){
+     return `<li class="gallery-item">
   <a class="gallery-link" href="${original}"> 
     <img class="gallery-image"
       src="${preview}"
-      data-source="${preview}"
+      data-source="${original}"
       alt="${description}"
     />
   </a>
@@ -105,12 +97,12 @@ const galleryContainer = document.querySelector('.gallery');
 galleryContainer.addEventListener('click', e => {
     e.preventDefault()
     if (e.target === e.currentTarget) return;
-  const imgId = e.target.getAttribute('data-id');
+  const preview = e.target.getAttribute('data-source');
   
   
     
 const instance = basicLightbox.create(`
-    <img src="${preview}" width="1112" height="640" data-id ="${id}>
+    <img src="${preview}" width="1112" height="640">
 `,
  {
      onShow: instance => {
